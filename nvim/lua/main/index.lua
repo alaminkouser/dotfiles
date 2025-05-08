7feff12de34788e5f3d1ec867941f533ade1d7d3
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-05-08 16:09:18 +0600

    added: nvim base

---
+++ nvim/lua/main/index.lua
@@ -0,0 +1,23 @@
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.wo.relativenumber = true
+vim.cmd [[set numberwidth=1]]
+vim.cmd [[let &stc="%=%{v:relnum?v:relnum:v:lnum} "]]
+
+vim.cmd [[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight Normal ctermbg=none
+  highlight NonText ctermbg=none
+]]
+
+vim.cmd [[set cmdheight=0]]
+
+vim.cmd [[set mouse=a]]
+vim.cmd [[aunmenu PopUp.-1-]]
+vim.cmd [[aunmenu PopUp.How-to\ disable\ mouse]]
+
+vim.cmd [[set signcolumn=no]]
---
+++ nvim/lua/main/init.lua
@@ -0,0 +1,8 @@
+require("main.lazy")
+
+require("main.lspconfig")
+require("main.nvim-cmp")
+
+require("main.index")
+
+
