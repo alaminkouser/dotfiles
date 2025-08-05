2228a086f610b7665f5cde37b0d9ece0e6c26378
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-08-06 02:19:37 +0600

    fixed: empty buffer

--- nvim/lua/main/config/yazi.lua
+++ nvim/lua/main/config/yazi.lua
@@ -1,17 +1,19 @@
 require("yazi").setup({
   open_for_directories = true,
   hooks = {
-    yazi_closed_successfully = function()
-      if vim.v.vim_did_enter == 0 then return end
+    yazi_closed_successfully = function(chosen_file)
+      if chosen_file == nil then
+	if vim.v.vim_did_enter == 0 then return end
 
-      local buf = vim.api.nvim_get_current_buf()
-      if not vim.api.nvim_buf_is_loaded(buf) then return end
+        local buf = vim.api.nvim_get_current_buf()
+        if not vim.api.nvim_buf_is_loaded(buf) then return end
 
-      local name = vim.api.nvim_buf_get_name(buf)
-      local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+        local name = vim.api.nvim_buf_get_name(buf)
+        local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
 
-      if name == "" and #lines == 1 and lines[1] == "" then
-	vim.cmd("q")
+        if name == "" and #lines == 1 and lines[1] == "" then
+	  vim.cmd("q")
+        end
       end
     end
   }
