fd193927ad1aac11eb68f661f7676c0a2d9edadb
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-08-06 23:52:30 +0600

    fixed: treesiter

--- nvim/lua/main/pack.lua
+++ nvim/lua/main/pack.lua
@@ -6,7 +6,6 @@ vim.pack.add({
   },
   {
     src = "https://github.com/nvim-treesitter/nvim-treesitter",
-    version = "main",
   },
   { src = "https://github.com/lewis6991/gitsigns.nvim" },
   { src = "https://github.com/catppuccin/nvim" },
