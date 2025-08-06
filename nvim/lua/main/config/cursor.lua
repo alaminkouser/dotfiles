a5eff74a0dbd8d39c5f5d225ec4edca65017f97b
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-08-06 12:42:58 +0600

    wrap texts

--- nvim/lua/main/config/cursor.lua
+++ nvim/lua/main/config/cursor.lua
@@ -1,1 +1,2 @@
 vim.opt.guicursor = { "i-c-ci:ver25-blinkwait500-blinkon300-blinkoff300" }
+vim.cmd("set virtualedit+=onemore")
--- nvim/lua/main/config/gutter.lua
+++ nvim/lua/main/config/gutter.lua
@@ -1,3 +1,5 @@
 vim.wo.relativenumber = true
 vim.opt.numberwidth = 1
 vim.cmd("let &stc=\"%=%{v:relnum?v:relnum:v:lnum} \"")
+
+vim.opt.wrap = false
