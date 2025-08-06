3edcd7abd81d7f40dbc99514054912c4243e1a1c
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-08-06 17:30:50 +0600

    added: Diff Command

--- nvim/lua/main/config/commands.lua
+++ nvim/lua/main/config/commands.lua
@@ -1,3 +1,10 @@
 vim.api.nvim_create_user_command("E", function()
   vim.cmd("Yazi")
 end, { desc = "Yazi" })
+
+vim.api.nvim_create_user_command("Diff", function()
+  vim.cmd([[
+    Gitsigns toggle_linehl
+    Gitsigns toggle_word_diff
+  ]])
+end, { desc = "Toggle Git Diff" })
