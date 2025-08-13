61b97626aa307b4aa3ce2dc2487b65e6019c6333
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-08-13 20:25:47 +0600

    update

--- nvim/lua/main/config/style.lua
+++ nvim/lua/main/config/style.lua
@@ -17,6 +17,10 @@ require("catppuccin").setup({
 })
 
 vim.cmd.colorscheme("catppuccin")
+local palette = require("catppuccin.palettes").get_palette()
+
+vim.opt.colorcolumn = "80"
+vim.api.nvim_set_hl(0, "ColorColumn", { bg = palette.crust })
 
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
