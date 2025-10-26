86482095e691b50783f4751a3ca9e25c6de1e257
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-10-26 13:24:39 +0600

    UPDATE

--- nvim/lua/main/pack.lua
+++ nvim/lua/main/pack.lua
@@ -5,6 +5,7 @@ vim.pack.add({
     src = "https://github.com/saghen/blink.cmp",
     version = vim.version.range("*"),
   },
+  { src = "https://github.com/MagicDuck/grug-far.nvim" },
   {
     src = "https://github.com/nvim-treesitter/nvim-treesitter",
   },
