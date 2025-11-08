419eb0bbe40951edf62b4365f9ddb0b4d4693532
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-11-09 02:01:27 +0600

    update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,21 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # To-Do: Needs curl, so check check curl installation and download curl if not found.
+
+# mkdir -p $HOME/.local/bin/miniconda
+# curl "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh" \
+#   --progress-bar \
+#   -o "$HOME/.local/bin/miniconda/miniconda.sh"
+# bash "$HOME/.local/bin/miniconda/miniconda.sh" -b -u -p "$HOME/.local/bin/miniconda"
+# rm "$HOME/.local/bin/miniconda/miniconda.sh"
+# source "$HOME/.local/bin/miniconda/bin/activate"
+# conda init bash
+conda install \
+  conda-forge::openssh \
+  anaconda::git \
+  conda-forge::gnupg \
+  conda-forge::yazi \
+  --yes
 
 curl -sS https://starship.rs/install.sh | \
   sh -s -- --bin-dir $HOME/.local/bin --yes
@@ -12,3 +27,6 @@ curl "https://raw.githubusercontent.com/junegunn/fzf/refs/heads/master/bin/fzf-p
   -o "$HOME/.local/bin/fzf-preview.sh" \
   --progress-bar
 chmod 700 "$HOME/.local/bin/fzf-preview.sh"
+
+
+
