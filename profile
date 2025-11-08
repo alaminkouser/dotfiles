7a636bfada3b3aa54808bc1f8ae6cf2732407c1b
AL AMIN KOUSER <alaminkouser@icloud.com>
2025-11-08 15:05:37 +0600

    update

--- profile
+++ profile
@@ -6,7 +6,7 @@ fi
 
 if [ -z "$WAYLAND_DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ] ; then
   gpgconf --launch gpg-agent
-  sshd -f $HOME/.ssh/sshd/sshd_config
+  $HOME/.local/bin/miniconda/bin/sshd -f $HOME/.ssh/sshd/sshd_config
   pg_ctl -D $HOME/FILES/DATABASE -l /dev/null start  
   exec sway
 fi
--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+# To-Do: Needs curl, so check check curl installation and download curl if not found.
+
+curl -sS https://starship.rs/install.sh | \
+  sh -s -- --bin-dir $HOME/.local/bin --yes
+
+curl -fsSL https://tombi-toml.github.io/tombi/install.sh | sh
+
 printf "Downloading fzf-preview.sh\n"
 curl "https://raw.githubusercontent.com/junegunn/fzf/refs/heads/master/bin/fzf-preview.sh" \
   -o "$HOME/.local/bin/fzf-preview.sh" \
