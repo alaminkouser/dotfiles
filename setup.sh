c0ed1bd28cf900e63c332101e94e17d5f06dee13
AL AMIN KOUSER <alaminkouser@icloud.com>
2026-02-17 00:59:07 +0600

    gpg

--- setup.sh
+++ setup.sh
@@ -27,3 +27,4 @@ curl "https://raw.githubusercontent.com/junegunn/fzf/refs/heads/master/bin/fzf-p
   -o "$HOME/.local/bin/fzf-preview.sh" \
   --progress-bar
 chmod 700 "$HOME/.local/bin/fzf-preview.sh"
+
