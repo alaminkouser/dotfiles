138e28ace998559cfac0ca871d060f4721e63792
AL AMIN KOUSER <alaminkouser@icloud.com>
2026-05-17 10:58:53 +0600

    .

---
+++ base.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+conda install\
+  conda-forge::aria2\
+  conda-forge::btop\
+  conda-forge::cloudflare\
+  conda-forge::eza\
+  conda-forge::fastfetch\
+  conda-forge::gh\
+  conda-forge::markdownlint-cli2\
+  conda-forge::nvim\
+  conda-forge::openssh\
+  conda-forge::starship\
+  conda-forge::yazi\
+  conda-forge::zellij\
+  conda-forge::zig\
+  conda-forge::zoxide\
+  main::curl\
+  main::ffmpeg\
+  main::file\
+  main::fzf\
+  main::git\
+  main::jq\
+  main::nodejs\
+  --yes
