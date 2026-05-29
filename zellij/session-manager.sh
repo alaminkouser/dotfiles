3849f0e374d41e748eb3fdfe973b97be0e56cb34
AL AMIN KOUSER <alaminkouser@icloud.com>
2026-05-30 00:25:25 +0600

    feat(zellij): Adjust UI, pane defaults, and session manager UX

--- zellij/session-manager.sh
+++ zellij/session-manager.sh
@@ -4,7 +4,7 @@ gum style --foreground "#FFF" --align center "SESSION MANAGER"
 
 session=$(
     printf "CREATE NEW SESSION\n%s" "$(zellij list-sessions --short | sort)" 2>/dev/null |
-    gum filter --no-show-help --header "CHOOSE:"
+    gum filter --no-show-help --header "SESSION MANAGER" --header.foreground "#FFF"
 )
 
 [ -z "$session" ] && exit 0
@@ -13,6 +13,5 @@ if [ "$session" = "CREATE NEW SESSION" ]; then
     session=$(gum input --cursor.foreground "#FFF" --placeholder "Enter new session name..." --no-show-help)
     [ -z "$session" ] && exit 0
 fi
-
 
 zellij action switch-session $session
